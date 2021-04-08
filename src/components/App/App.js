b37900382d7585a124af100da9075453326d70d4
Max Ivanov <waxivanov@gmail.com>
2021-04-08 15:52:22 +0300

    init cra, structure, included styles and fonts

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Hello</h1>
+      <h2 className={styles.subtitle}>Torba</h2>
+    </div>
+  );
+};
+
+export default App;
