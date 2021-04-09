c79f1e80431bd24af9aa1f3b74ec9186dc7ba959
Max Ivanov <waxivanov@gmail.com>
2021-04-09 16:02:44 +0300

    windowSizeHook start

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,28 @@
+import { useState, useEffect } from 'react';
 import Header from '../Header';
 import Intro from '../Intro';
-import SliderMobile from '../SliderMobile';
+// import SliderMobile from '../SliderMobile';
+// import SliderDesktop from '../SliderDesktop';
+import Slider from '../Slider';
 import Products from '../Products';
 import NewsLetter from '../NewsLetter';
 import Footer from '../Footer';
-import SliderDesktop from '../SliderDesktop';
 
 import styles from './App.module.css';
 
+import useWindowSize from '../../hooks/useWindowSize';
+
 const App = () => {
+  const [isMobile, setIsMobile] = useState(true);
+  const size = useWindowSize();
+
   return (
     <div className={styles.wrapper}>
-      <SliderDesktop />
+      {isMobile ? null : <Slider />}
       <div className={styles.wrapper_inner}>
         <Header />
         <Intro />
-        <SliderMobile />
+        {isMobile ? <Slider /> : null}
         <Products />
         <NewsLetter />
         <Footer />
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,11 @@
+import styles from './Slider.module.css';
+
+const Slider = () => {
+  return (
+    <div className={styles.slider}>
+      <h2>Slider</h2>
+    </div>
+  );
+};
+
+export default Slider;
