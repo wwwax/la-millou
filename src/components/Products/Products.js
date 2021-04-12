d3e17acfaca7334f0a9fe6d3d553e04f4b589e96
Max Ivanov <waxivanov@gmail.com>
2021-04-12 13:41:53 +0300

    products

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,31 +2,78 @@ import styles from './Products.module.css';
 
 const Products = () => {
   return (
-    <div className={styles.products}>
-      <h2 className={styles.title}>Products</h2>
+    <div className={styles.section}>
+      <div className='container'>
+        <p className={styles.text}>
+          <span className={styles.bold}>Для нас важливо знати,</span> які продукти ви б
+          хотіли мати змогу придбати в першу чергу. Виберіть будь-ласка зі списку ті
+          продукти LaMillou, які повинні бути в продажі на вашу думку як найшвидше
+        </p>
 
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus assumenda
-        asperiores dolores a exercitationem facilis nesciunt inventore eum pariatur
-        reiciendis repellat, minima vel voluptas voluptatibus ipsum blanditiis delectus
-        harum saepe, distinctio obcaecati! Laboriosam similique accusantium blanditiis
-        nam? Sint ut ipsa expedita vitae voluptas, nostrum explicabo maiores sequi quas
-        sit veritatis nobis quis mollitia iure et facilis repellat consectetur eius neque
-        autem sapiente dolorem. Omnis sunt, magnam nobis illo, animi distinctio odit
-        exercitationem qui ipsa corrupti dicta eum. Reiciendis earum quas itaque nobis
-        facilis. Quia laudantium, numquam repellendus corrupti commodi officiis quos natus
-        aut, maxime est optio nisi tempore eligendi facere, sapiente animi? Quidem rem
-        quisquam iure omnis ipsum. Ipsum voluptates hic, reiciendis atque ratione
-        quibusdam quidem quisquam consequatur inventore ex possimus adipisci cum quaerat
-        sit nemo soluta autem minus ut repudiandae consequuntur. At reiciendis, excepturi
-        error id, incidunt molestiae laboriosam ea harum aperiam consequatur commodi quis
-        fuga numquam temporibus dolores reprehenderit hic esse adipisci consequuntur quod,
-        mollitia eos fugit! Obcaecati, culpa voluptates? Possimus, eaque omnis, iure
-        laboriosam adipisci rem deleniti itaque, numquam beatae quibusdam quisquam
-        cupiditate accusantium? Numquam quod maxime voluptatibus maiores quo unde eos quas
-        labore sequi, rem iste, atque inventore ab sunt aperiam odit tempore doloribus
-        similique hic?
-      </p>
+        <form className={styles.form}>
+          <div className={styles.productList}>
+            <div className={styles.productListItem}>
+              <input
+                className={styles.input}
+                type='checkbox'
+                id='blankets'
+                name='products'
+              />
+              <label className={styles.label} for='blankets'>
+                ковдри
+              </label>
+            </div>
+
+            <div className={styles.productListItem}>
+              <input
+                className={styles.input}
+                type='checkbox'
+                id='baby-clothes'
+                name='products'
+              />
+              <label className={styles.label} for='baby-clothes'>
+                дитячий одяг
+              </label>
+            </div>
+
+            <div className={styles.productListItem}>
+              <input
+                className={styles.input}
+                type='checkbox'
+                id='pillows'
+                name='products'
+              />
+              <label className={styles.label} for='pillows'>
+                подушки
+              </label>
+            </div>
+
+            <div className={styles.productListItem}>
+              <input
+                className={styles.input}
+                type='checkbox'
+                id='towels-bathrobes'
+                name='products'
+              />
+              <label className={styles.label} for='towels-bathrobes'>
+                рушники і халати
+              </label>
+            </div>
+
+            <div className={styles.productListItem}>
+              <input
+                className={styles.input}
+                type='checkbox'
+                id='bags-backpacks'
+                name='products'
+              />
+              <label className={styles.label} for='bags-backpacks'>
+                торби і рюкзаки
+              </label>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
