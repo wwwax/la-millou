3ef58da729bddbb74b90a179943d491817739be0
Max Ivanov <waxivanov@gmail.com>
2021-04-12 11:13:47 +0300

    adaptive slider

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,24 @@
-import { useState, useEffect } from 'react';
 import Header from '../Header';
 import Intro from '../Intro';
-// import SliderMobile from '../SliderMobile';
-// import SliderDesktop from '../SliderDesktop';
 import Slider from '../Slider';
 import Products from '../Products';
 import NewsLetter from '../NewsLetter';
 import Footer from '../Footer';
 
-import styles from './App.module.css';
-
 import useWindowSize from '../../hooks/useWindowSize';
 
+import styles from './App.module.css';
+
 const App = () => {
-  const [isMobile, setIsMobile] = useState(true);
-  const size = useWindowSize();
+  const windowSize = useWindowSize();
 
   return (
     <div className={styles.wrapper}>
-      {isMobile ? null : <Slider />}
+      {windowSize.width < 1024 ? null : <Slider />}
       <div className={styles.wrapper_inner}>
         <Header />
         <Intro />
-        {isMobile ? <Slider /> : null}
+        {windowSize.width < 1024 ? <Slider /> : null}
         <Products />
         <NewsLetter />
         <Footer />
