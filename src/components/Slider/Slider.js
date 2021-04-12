34895843c4d6cb0489a6d593d4da7695799be948
Max Ivanov <waxivanov@gmail.com>
2021-04-12 16:52:53 +0300

    tooltip boilerplate

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,8 +1,10 @@
+import Tooltip from '../Tooltip';
 import styles from './Slider.module.css';
 
 const Slider = () => {
   return (
     <div className={styles.slider}>
+      <Tooltip />
       <h2>Slider</h2>
     </div>
   );
---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,11 @@
+import styles from './Tooltip.module.css';
+
+const Tooltip = () => {
+  return (
+    <div className={styles.tooltip}>
+      <div>Tooltip</div>
+    </div>
+  );
+};
+
+export default Tooltip;
