090833d2147db0a85073df841f56a7ea98616b0d
Max Ivanov <waxivanov@gmail.com>
2021-04-16 15:41:03 +0300

    tooltip

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,5 +1,4 @@
 import SlickSlider from 'react-slick';
-import Tooltip from '../Tooltip';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -8,8 +7,6 @@ import styles from './Slider.module.css';
 import img_1 from '../../img/slider/image-1.jpg';
 import img_2 from '../../img/slider/image-2.jpg';
 import img_3 from '../../img/slider/image-3.jpg';
-
-import ico from '../../img/bag.svg';
 
 const Slider = () => {
   const settings = {
@@ -29,18 +26,39 @@ const Slider = () => {
     <div className={styles.slider}>
       <SlickSlider {...settings}>
         <div className={styles.sliderItem}>
-          <Tooltip name='Torba' brand='Dolce Vita' ico={ico} />
           <img className={styles.sliderImage} src={img_1} alt='' />
+
+          <div className={styles.tooltip}>
+            <div className={styles.pulseButton}></div>
+            <div className={styles.tooltipContent}>
+              <div className={styles.name}>Torba</div>
+              <div className={styles.brand}>Dolce Vita</div>
+            </div>
+          </div>
         </div>
 
         <div className={styles.sliderItem}>
-          <Tooltip name='lorem ipsum dolor' brand='lorem ipsum dolor' ico={ico} />
           <img className={styles.sliderImage} src={img_2} alt='' />
+
+          <div className={styles.tooltip}>
+            <div className={styles.pulseButton}></div>
+            <div className={styles.tooltipContent}>
+              <div className={styles.name}>Shirt</div>
+              <div className={styles.brand}>Dolce Gabbana</div>
+            </div>
+          </div>
         </div>
 
         <div className={styles.sliderItem}>
-          <Tooltip name='lorem ipsum dolor' brand='lorem ipsum dolor' ico={ico} />
           <img className={styles.sliderImage} src={img_3} alt='' />
+
+          <div className={styles.tooltip}>
+            <div className={styles.pulseButton}></div>
+            <div className={styles.tooltipContent}>
+              <div className={styles.name}>Hat</div>
+              <div className={styles.brand}>Louis Vuitton</div>
+            </div>
+          </div>
         </div>
       </SlickSlider>
     </div>
