81f138002cd6e8b43d0321c1f542f5ce80f8f264
Max Ivanov <waxivanov@gmail.com>
2021-04-21 15:26:54 +0300

    demo

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,101 +1,8 @@
 import SlickSlider from 'react-slick';
-
+import { sliderItems } from './config';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from './Slider.module.css';
-
-import img_1 from '../../img/slider/image-1.jpg';
-import img_2 from '../../img/slider/image-2.jpg';
-import img_3 from '../../img/slider/image-3.jpg';
-
-const sliderItems = [
-  {
-    id: 1,
-    src: img_1,
-    imageObjectPosition: 'top',
-    tooltips: [
-      {
-        id: 1,
-        name: 'Torba',
-        brand: 'Dolce Vita',
-        position: {
-          left: '50%',
-          top: '50%',
-        },
-      },
-    ],
-  },
-
-  {
-    id: 2,
-    src: img_2,
-    imageObjectPosition: 'bottom',
-    tooltips: [
-      {
-        id: 1,
-        name: 'Big John',
-        brand: 'Navy Jungle',
-        position: {
-          left: '70%',
-          top: '50%',
-        },
-      },
-      {
-        id: 2,
-        name: 'Kocyk średniaka',
-        brand: 'Navy Jungle',
-        position: {
-          left: '30%',
-          top: '80%',
-        },
-      },
-      {
-        id: 3,
-        name: 'Koc narzuta',
-        brand: 'Navy Jungle',
-        position: {
-          left: '80%',
-          top: '80%',
-        },
-      },
-    ],
-  },
-
-  {
-    id: 3,
-    src: img_3,
-    imageObjectPosition: 'center',
-    tooltips: [
-      {
-        id: 1,
-        name: 'Bambusowa Kura Babci Dany',
-        brand: 'Boho Palms Light',
-        position: {
-          left: '35%',
-          top: '40%',
-        },
-      },
-      {
-        id: 2,
-        name: 'Bamboo Bedding Medium',
-        brand: 'Boho Girl',
-        position: {
-          left: '55%',
-          top: '60%',
-        },
-      },
-      {
-        id: 3,
-        name: 'Organic Jersey Collection - KOMPLET POŚCIELI "M" 80X100',
-        brand: 'Boho Girl',
-        position: {
-          left: '80%',
-          top: '70%',
-        },
-      },
-    ],
-  },
-];
 
 const Slider = () => {
   const settings = {
@@ -104,11 +11,11 @@ const Slider = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 7000,
     fade: true,
     speed: 2000,
-    pauseOnHover: false,
+    pauseOnHover: true,
   };
 
   return (
@@ -134,7 +41,13 @@ const Slider = () => {
                   key={tooltipItem.id}>
                   <div className={styles.pulseButton}></div>
                   <div className={styles.tooltipContent}>
-                    <div className={styles.name}>{tooltipItem.name}</div>
+                    <a
+                      className={styles.name}
+                      href={tooltipItem.productLink}
+                      target='_blank'
+                      rel='noopener noreferrer'>
+                      {tooltipItem.name}
+                    </a>
                     <div className={styles.brand}>{tooltipItem.brand}</div>
                   </div>
                 </div>
---
+++ src/components/Slider/config.js
@@ -0,0 +1,102 @@
+import img_1 from '../../img/slider/image-1.jpg';
+import img_2 from '../../img/slider/image-2.jpg';
+import img_3 from '../../img/slider/image-3.jpg';
+
+export const sliderItems = [
+  {
+    id: 1,
+    src: img_1,
+    imageObjectPosition: 'top',
+    tooltips: [
+      {
+        id: 1,
+        name: 'Torba',
+        brand: 'Dolce Vita',
+        productLink: 'https://www.lamillou.com/shop/dolce-vita-pure-plecak-arizona',
+        position: {
+          left: '50%',
+          top: '50%',
+        },
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    src: img_2,
+    imageObjectPosition: 'bottom',
+    tooltips: [
+      {
+        id: 1,
+        name: 'Big John',
+        brand: 'Navy Jungle',
+        productLink: 'https://www.lamillou.com/shop/big-john-navy-jungle-ecru',
+        position: {
+          left: '70%',
+          top: '50%',
+        },
+      },
+      {
+        id: 2,
+        name: 'Kocyk średniaka',
+        brand: 'Navy Jungle',
+        productLink: 'https://www.lamillou.com/shop/kocyk-sredniaka-navy-jungle-ecru',
+        position: {
+          left: '30%',
+          top: '80%',
+        },
+      },
+      {
+        id: 3,
+        name: 'Koc narzuta',
+        brand: 'Navy Jungle',
+        productLink:
+          'https://www.lamillou.com/shop/koc-narzuta-140-x-200-cm-navy-jungle-ecru',
+        position: {
+          left: '80%',
+          top: '80%',
+        },
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    src: img_3,
+    imageObjectPosition: 'center',
+    tooltips: [
+      {
+        id: 1,
+        name: 'Bambusowa Kura Babci Dany',
+        brand: 'Boho Palms Light',
+        productLink:
+          'https://www.lamillou.com/shop/bambusowa-kura-babci-dany-boho-palms-light-deep-ocean',
+        position: {
+          left: '35%',
+          top: '40%',
+        },
+      },
+      {
+        id: 2,
+        name: 'Bamboo Bedding Medium',
+        brand: 'Boho Girl',
+        productLink: 'https://www.lamillou.com/shop/bamboo-bedding-medium-size-boho-girl',
+        position: {
+          left: '55%',
+          top: '60%',
+        },
+      },
+      {
+        id: 3,
+        name: 'Organic Jersey Collection - KOMPLET POŚCIELI "M" 80X100',
+        brand: 'Boho Girl',
+        productLink:
+          'https://www.lamillou.com/shop/organic-jersey-collection-komplet-poscieli-m-80x100-boho-girl',
+        position: {
+          left: '80%',
+          top: '70%',
+        },
+      },
+    ],
+  },
+];
